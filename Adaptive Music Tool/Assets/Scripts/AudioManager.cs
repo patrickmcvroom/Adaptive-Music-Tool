223f1b96fed689d23a6b997b181dc9ecb5891aac
Patrick McVroom <patrickmcvroom@gmail.com>
2020-05-01 01:54:04 +0100

    Adaptive Music Tool Version 1

---
+++ Adaptive Music Tool/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioManager : MonoBehaviour
+{
+    public HeartRateManager heartRateManager;
+
+    public List<AudioClip> trackOne;
+    public List<AudioClip> trackTwo;
+    public List<AudioClip> trackThree;
+
+    public Button trackOneButton;
+    public Button trackTwoButton;
+    public Button trackThreeButton;
+    public Button stopButton;
+
+    private AudioSource source;
+    private int currentIntensity;
+    private Tracks currentTrack;
+
+    private enum Tracks {One, Two, Three };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        trackOneButton.onClick.AddListener(() =>
+        {
+            PlayMusic(Tracks.One);
+        });
+        trackTwoButton.onClick.AddListener(() =>
+        {
+            PlayMusic(Tracks.Two);
+        });
+        trackThreeButton.onClick.AddListener(() =>
+        {
+            PlayMusic(Tracks.Three);
+        });
+
+        stopButton.onClick.AddListener(source.Stop);
+    }
+
+    private void OnDestroy()
+    {
+        trackOneButton.onClick.RemoveAllListeners();
+        trackTwoButton.onClick.RemoveAllListeners();
+        trackThreeButton.onClick.RemoveAllListeners();
+
+        stopButton.onClick.RemoveAllListeners();
+    }
+
+    private void PlayMusic(Tracks trackName, bool restart = true)
+    {
+        AudioClip clip = null;
+
+        switch(trackName)
+        {
+            case Tracks.One:
+                if(trackOne.Count == 0)
+                {
+                    return;
+                }
+                clip = trackOne[currentIntensity];
+                break;
+            case Tracks.Two:
+                if (trackTwo.Count == 0)
+                {
+                    return;
+                }
+                clip = trackTwo[currentIntensity];
+                break;
+            case Tracks.Three:
+                if (trackThree.Count == 0)
+                {
+                    return;
+                }
+                clip = trackThree[currentIntensity];
+                break;
+        }
+
+        currentTrack = trackName;
+
+        PlayAudioClip(clip, restart);
+        Debug.Log($"Playing {currentTrack}");
+    }
+
+    private void PlayAudioClip(AudioClip clip, bool restart = false)
+    {
+        if(clip == null)
+        {
+            return;
+        }
+
+        var currentTime = 0f;
+
+        if (source.isPlaying && !restart)
+        {
+            currentTime = source.time;
+            source.Stop();
+        }
+
+        source.clip = clip;
+
+        source.time = currentTime;
+        source.loop = true;
+        source.Play();
+
+
+    }
+
+    private void Update()
+    {
+        if (currentIntensity == heartRateManager.intensity)
+        {
+            return;
+        }
+
+        currentIntensity = heartRateManager.intensity;
+
+        PlayMusic(currentTrack, false);
+    }
+}
---
+++ Adaptive Music Tool/Assets/Scripts/HeartRateManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO.Ports;
+using System.Linq;
+
+public class HeartRateManager : MonoBehaviour
+{
+    const int queueSize = 10;
+
+    private readonly SerialPort sp = new SerialPort("/dev/cu.usbmodem141201", 9600);
+    private readonly Queue<int> calibrationQueue = new Queue<int>(queueSize);
+
+    public int restingHeartRate;
+    public int currentHeartRate;
+    [Range(0, 4)]
+    public int intensity;
+    public float readInterval = 1;
+
+    public TextMeshProUGUI currentHRText;
+    public TextMeshProUGUI restingHRText;
+    public Button calibrateButton;
+    public bool isCalibrating;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = (int)(readInterval * 1000);
+
+        StartCoroutine(ReadPort());
+
+        calibrateButton.onClick.AddListener(() =>
+        {
+            calibrationQueue.Clear();
+            isCalibrating = true;
+        });
+    }
+
+    private void OnDestroy()
+    {
+        calibrateButton.onClick.RemoveAllListeners();
+        sp.Close();
+        StopCoroutine(ReadPort());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calibrateButton.interactable = !isCalibrating;
+
+        currentHRText.text = $"Current Heart Rate: {currentHeartRate}";
+        restingHRText.text = $"Resting Heart Rate: {restingHeartRate}";
+
+        intensity = Mathf.RoundToInt(intensity);
+
+        if (currentHeartRate <= restingHeartRate)
+        {
+            intensity = 0;
+        }
+
+        if (currentHeartRate >= restingHeartRate + 2)
+        {
+            intensity = 1;
+        }
+
+        if (currentHeartRate >= restingHeartRate + 4)
+        {
+            intensity = 2;
+        }
+
+        if (currentHeartRate >= restingHeartRate + 6)
+        {
+            intensity = 3;
+        }
+
+        if (currentHeartRate >= restingHeartRate + 8)
+        {
+            intensity = 4;
+        }
+    }
+
+    private IEnumerator ReadPort()
+    {
+        yield return new WaitForSeconds(readInterval);
+
+        if (sp.IsOpen)
+        {
+            var value = sp.ReadByte();
+            currentHeartRate = value;
+
+            if(isCalibrating)
+            {
+                Calibrate(currentHeartRate);
+            }
+        }
+
+        StartCoroutine(ReadPort());
+
+    }
+
+    private void Calibrate(int heartrate)
+    {
+        if(calibrationQueue.Count == queueSize)
+        {
+            calibrationQueue.Dequeue();
+            isCalibrating = false;
+        }
+        calibrationQueue.Enqueue(heartrate);
+        restingHeartRate = calibrationQueue.Sum() / calibrationQueue.Count;
+    }
+}
